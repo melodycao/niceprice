2623d5783725219de8a7a11273f18b9ea9f049fe
SophieWorks <sophiework05@gmail.com>
2021-01-17 00:44:58 -0500

    commit

---
+++ landing.js
@@ -0,0 +1,19 @@
+
+
+
+// var input = document.getElementById("searchEnter");
+// input.addEventListener("keyup", function(event) {
+//   if (event.keyCode === 13) {
+//    event.preventDefault();
+//    document.getElementById("myBtn").click();
+
+//   }
+// });
+
+
+
+function searchEnter() {
+
+
+    
+}
